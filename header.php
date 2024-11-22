9dcae40ce331ed0b7d6d518a16a1621cf9d83c1b
@rickhogenhout <2385-rickhogenhout@users.noreply.bitlab.bit-academy.nl>
2024-11-22 10:24:31 +0100

    Beginning of the project

---
+++ header.php
@@ -0,0 +1,27 @@
+<header class="main-header">
+        <nav class="navbar">
+            <div class="logo">DragonBlaze</div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">My Youtube channel</a></li>
+                <li><a href="#">Top Level e-sports</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropbtn">Dutch Pokemon Story</a>
+                    <div class="dropdown-content">
+                        <a href="#">DPS S1:Part 1</a>
+                        <a href="#">DPS S1:Part 2</a>
+                        <a href="#">DPS S2:Part 1</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropbtn">Movie Nights</a>
+                    <div class="dropdown-content">
+                        <a href="#">Movies seen</a>
+                        <a href="#">Movie Rankings</a>
+                        <a href="#">Your movie rankings</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+</header>
+    <div class="banner-image"></div>
---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Official Dragon Blaze Website">
+    <title>Dragon Blaze</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="img/image-removebg-preview.png" type="image/x-icon">
+</head>
+<body>
+    <?php include_once "header.php" ?> 
+
+
+
+    <main class="hero-section">
+        <h1>Welcome to the home page!</h1>
+        <p> I am DragonBlaze! a.k.a Andrew!</p> 
+        <p>I'm a YouTuber/Twitch streamer who likes to have fun and entertain people! </p>
+<p>Used to be all about Inazuma Eleven on YT and now I'm playing basically whatever. Usually something on the Nintendo Switch.</p> 
+<p>Hope you guys will enjoy this page, it is pretty much going to be about.. well.. me!</p> 
+<p>Going to make it more personal to have my subs know who I am and stuff and hopefully it will be a fun time!</p> 
+<p>I will try and update this page as much as I can!</p>
+<p>If there ever is a idea of a certain page you would like to see on my Website, just say it.</p>
+<p>This is a page for myself, but also like I said for my subscribers, so if there ever would be a thing that you would like to know, just say so and I'll think about it if I will add it to the website! :] </p>
+<p>Hopefully you guys will all enjoy and the way of talking to me if there ever would be a moment when you want to advice me something on the page can be done through Discord!</p> 
+</main>
+<div class="pages">
+    <a href="#" class="button"> <span>GO DIRECTLY TO MY YOUTUBE PAGE</span> </a>
+    <a href="#" class="button"> <span>GO DIRECTLY TO MY TWITCH PAGE</span></a>
+    <a href="#" class="button"> <span>GO DIRECTLY TO MY DISCORD</span> </a>
+</div>
+</body>
+</html>
